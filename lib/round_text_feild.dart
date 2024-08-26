0052b9b7047adc2d0dd7488f41505f63cab77541
subash Gaddam <gaddamsubashreddy@gmail.com>
2024-08-26 03:53:51 -0400

    modified round text feild and clearing bugs and errors

--- lib/round_text_feild.dart
+++ lib/round_text_feild.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meditation/common/color_extension.dart';
+import 'package:meditation_app_flutterfinalproject/color_extension.dart';
 
 class RoundTextField extends StatelessWidget {
   final String hintText;
